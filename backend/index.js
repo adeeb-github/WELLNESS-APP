e0484b6d00f7d0d5744c675d6f1ab16db06e7258
adeeb-github <112182422+adeeb-github@users.noreply.github.com>
2025-08-03 14:54:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin:"http://localhost:3000",
+        origin:"*",
         credentials:true,
         methods:["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type","Authorization"],
