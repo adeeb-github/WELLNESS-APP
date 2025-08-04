2aa032287c8f608b8753db294c9477cc83187622
adeeb-github <112182422+adeeb-github@users.noreply.github.com>
2025-08-04 09:15:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin:"https://soulnest-vsup.vercel.app/",
+        origin:"https://soulnest-vsup.vercel.app",
         credentials:true,
         methods:["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type","Authorization"],
